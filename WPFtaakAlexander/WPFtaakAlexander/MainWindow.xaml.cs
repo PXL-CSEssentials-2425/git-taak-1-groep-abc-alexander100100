f4a231f71169da3b913e50a6f3409a80bd649bcb
Alexander Mahrhofer <12401690@student.pxl.be>
2024-10-06 20:30:02 +0200

    KLEUR-03

--- WPFtaakAlexander/WPFtaakAlexander/MainWindow.xaml.cs
+++ WPFtaakAlexander/WPFtaakAlexander/MainWindow.xaml.cs
@@ -8,17 +8,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.IO;
+
 
 namespace WPFtaakAlexander
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
     {
-        public MainWindow()
+        /// <summary>
+        /// Interaction logic for MainWindow.xaml
+        /// </summary>
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+
+            private void pxlImage_MouseEnter(object sender, MouseEventArgs e)
+            {
+                mainWindow.Background = Brushes.Black;
+            }
+
+            private void pxlImage_MouseLeave(object sender, MouseEventArgs e)
+            {
+                mainWindow.Background = Brushes.White;
+
+            }
         }
     }
-}
