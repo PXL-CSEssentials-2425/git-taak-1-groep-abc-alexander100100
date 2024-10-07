3fb943dca5415415e563b76542873a8eff9d3656
alexander100100 <Alexander.Mahrhofer@student.pxl.be>
2024-10-07 16:45:20 +0200

    KLEUR-04

--- WPFtaakAlexander/WPFtaakAlexander/MainWindow.xaml.cs
+++ WPFtaakAlexander/WPFtaakAlexander/MainWindow.xaml.cs
@@ -14,26 +14,47 @@ using System.IO;
 
 namespace WPFtaakAlexander
 
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Interaction logic for MainWindow.xaml
-        /// </summary>
-        public partial class MainWindow : Window
+        public MainWindow()
         {
-            public MainWindow()
-            {
-                InitializeComponent();
-            }
+            InitializeComponent();
+        }
 
-            private void pxlImage_MouseEnter(object sender, MouseEventArgs e)
-            {
-                mainWindow.Background = Brushes.Black;
-            }
+        private void pxlImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.Black;
+        }
 
-            private void pxlImage_MouseLeave(object sender, MouseEventArgs e)
-            {
-                mainWindow.Background = Brushes.White;
+        private void pxlImage_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.White;
 
-            }
+        }
+
+        private void roodImage_Click(object sender, MouseButtonEventArgs e)
+        {
+            kleurShow.Text = "- Rood #FF0000";
+        }
+
+        private void geelImage_Click(object sender, MouseButtonEventArgs e)
+        {
+            kleurShow.Text = "- Geel #FFFF00";
+        }
+
+        private void blauwImage_Click(object sender, MouseButtonEventArgs e)
+        {
+            kleurShow.Text = "- Blauw: #0000FF";
+        }
+
+        private void groenImage_Click(object sender, MouseButtonEventArgs e)
+        {
+            kleurShow.Text = "- Groen: #008000";
         }
     }
+
+}
